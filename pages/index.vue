3425e8058dd4f4a7d6fa1c2d822311b8eaf2742b
PragmaticFox <rwitschi@moocloud.ch>
2022-04-17 11:14:17 +0200

    udpate

--- pages/index.vue
+++ pages/index.vue
@@ -1,100 +1,16 @@
 <template>
-  <canvas class="format-canvas-rendering" id="glCanvas">
-    Your browser does not seem to support HTML5 canvas.
-  </canvas>
-  <div class="format-div-fps">
-    fps: {{ (1.0 / context.deltaMean).toFixed(0) }}
+<div>
+  <a href="/test">test</a>
+  <br>
+  <a href="/game_of_life">game of life</a>
   </div>
 </template>
 
-<script setup lang="ts">
-const { $render } = useNuxtApp();
-</script>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      context: {
-        gl: null,
-        vertexShader: null,
-        vertexShaderSrc: null,
-        fragmentShader: null,
-        fragmentShaderSrc: null,
-        buffer: null,
-        program: null,
-        timeAnchor_ms: Date.now(),
-        then_ms: 0.0,
-        now_ms: 0.0,
-        deltaMean: 1.0,
-        deltaArr: [],
-        deltaSize: 1000,
-        verticesArr: [],
-        verticesSize: 100000,
-        isVerticesArrInitiated: false
-      },
-    };
-  },
-  mounted() {
-    const canvas = document.querySelector("#glCanvas");
-    if (canvas instanceof HTMLCanvasElement) {
-      this.context.gl = canvas.getContext("webgl2");
-    }
-    if (!this.context.gl) {
-      alert(
-        "Failed to get WebGL context. Your browser or device may not support WebGL."
-      );
-      return;
-    }
-     /*
-    this.context.gl.viewport(
-      0,
-      0,
-      this.context.gl.canvas.width,
-      this.context.gl.canvas.height
-    );
-    */
-    this.renderLoop();
-  },
-  methods: {
-    renderLoop() {
-      setTimeout(this.renderLoop, 1000 / 60);
-      this.$render(this.context);
-    },
-  },
-};
-</script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-
-html,
-body {
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  height: 100vh;
-  width: 100vw;
-}
-
-.format-canvas-rendering {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-}
-
-.format-div-fps {
-  left: 0;
-  top: 0;
-  font-size: 2vw;
-  z-index: 100;
-  color: white;
+<style scoped>
+div {
+  text-align: center;
+  position: fixed; /* or absolute */
+  top: 50%;
+  left: 50%;
 }
 </style>
---
+++ public/shaders/shader_2.vert
@@ -0,0 +1,27 @@
+#version 300 es
+
+float pi = 3.1415926538;
+
+uniform float u_delta;
+uniform mat4 u_camera;
+uniform float u_time;
+in vec3 vertex_coord;
+out vec4 color;
+
+float rand(vec2 p) {
+    return fract(sin(dot(p, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+void main() {
+    //float sin_in = mod(vertex_coord.z * 0.005 * u_time, 2.0 * pi);
+    //float cos_in = mod(vertex_coord.z * 0.005 * u_time, 2.0 * pi);
+    float x = vertex_coord.y * sin(vertex_coord.y * vertex_coord.z * 0.005 * u_time);
+    float y = vertex_coord.x * cos(vertex_coord.x * vertex_coord.z * 0.005 * u_time);
+    gl_Position = u_camera * vec4(x, y, 0.0, 1.0);
+    gl_PointSize = 3.0;
+    if(vertex_coord.z < 0.0) {
+        color = vec4(0.79, 0.15, 0.15, 1);
+    } else {
+        color = vec4(0.07, 0.15, 0.88, 1);
+    }
+}
