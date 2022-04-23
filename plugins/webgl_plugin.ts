130703a53131bff1de44dd764fdf05ae720999e4
Pragmatic Fox <ramon.witschi@alumni.ethz.ch>
2022-04-23 19:35:03 +0200

    simplified the api

--- plugins/webgl_plugin.ts
+++ plugins/webgl_plugin.ts
@@ -1,13 +1,9 @@
 import * as Po from "./webgl/particle_optimization";
 
 export default defineNuxtPlugin(() => {
-    return {
-        provide: {
-            Po: {
-                create: Po.create,
-                render: Po.render,
-                destroy: Po.destroy
-            }
-        }
-    }
-})
+  return {
+    provide: {
+      Po: Po,
+    },
+  };
+});
